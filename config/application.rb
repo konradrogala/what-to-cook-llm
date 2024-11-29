b24f2e72efd2fddc427ba27e131f1d7d10f31edb
konradrogala <konrad.rogala@windowslive.com>
2024-11-29 21:36:31 +0100

    add api limiter

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,10 @@ module WhatToCookLlm
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Add middleware directory to autoload paths
+    config.autoload_paths += %W(#{config.root}/app/middleware)
 
     # Configuration for the application, engines, and railties goes here.
     #
